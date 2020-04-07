5fcdf4789e39e38730ad73410d5fb1cf5049bbaf
nikojoel <niko-holopainen@hotmail.com>
2020-04-07 02:33:04 -0700

    Initial Commit

---
+++ WeaherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeaherApp
+//
+//  Created by Niko Holopainen on 7.4.2020.
+//  Copyright © 2020 Niko Holopainen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
